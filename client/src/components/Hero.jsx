ade99e13b1be4097eef2a68b8232311726c2eb49
deenoloo <doreenoloo2@gmail.com>
2025-08-12 11:16:53 -0400

    added the hero image

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,11 +2,11 @@ import { useContext } from "react"
 import { ApiContext } from "../context/ContextProvider"
 
 function Hero() {
-    const products = useContext(ApiContext)
+    const {assetUrl} = useContext(ApiContext)
     
     return(
         <div className="relative h-[80vh]">
-            <img className="w-full h-full object-contain" src={products[0]?.images[0] || "https://placehold.co/1920x900"} alt="Collection photo" />
+            <img className="w-full h-full object-contain" src={assetUrl('/images/hero.jpg') || "https://placehold.co/1920x900"} alt="Collection photo" />
             <div className="absolute bottom-30 w-full text-white">
                 <h2 className="text-center text-2xl z-10 p-5">
                     NAME OF COLLECTION
